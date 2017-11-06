f8e7fa8fa3aa1d24fb5463121e99f345a5a0bf07
Leah Gerald <Leanator13@gmail.com>
2017-11-06 11:31:31 -0500

    date and pwd made

--- bash.js
+++ bash.js
@@ -1,1 +1,23 @@
-console.log("it workes!")
+// Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', function (data) {
+    var cmd = data.toString().trim(); // remove the newline
+    if (cmd === "pwd") {
+        process.stdout.write(process.argv[0]);
+    }
+    if (cmd === "date"){
+        process.stdout.write(Date());
+    }
+    if (cmd === "cool") {
+        process.stdout.write("coool");
+    }
+
+    //process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+
+}
+
+
+);
